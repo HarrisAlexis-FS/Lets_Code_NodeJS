11b2020e0811b4c9d95a97fd2aa69436f00ef466
HarrisAlexis-FS <114839962+HarrisAlexis-FS@users.noreply.github.com>
2023-06-05 20:33:22 -0400

    Completed math test. All passed.

---
+++ math/math.js
@@ -0,0 +1,25 @@
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+const subtract = (num1, num2) => {
+    return num1-num2
+}
+
+const multiply = (num1, num2) => {
+    return num1-num2
+}
+
+const divide = (num1, num2) => {
+    return num1-num2
+}
+
+const sqrt = (num1) => {
+    return (Math.sqrt(num1))
+}
+
+const max = (num1, num2, num3, num4) => {
+    return (Math.max(num1, num2, num3, num4))
+}
+
+module.exports = {add, subtract, multiply, divide, sqrt, max}
--- math/math.test.js
+++ math/math.test.js
@@ -1,7 +1,30 @@
+const {add, subtract, multiply, divide, sqrt, max} = require("./math")
 describe("Testing the math module", () => {
 
+test("adding two numbers", () => {
+    const result = add(2,4);
+});
+
+test("subtracting two numbers", () => {
+    const result = subtract(4,2);
+});
+
+test("multiply two numbers", () => {
+    const result = multiply(4,2);
+});
+
+test("divide two numbers", () => {
+    const result = divide(4,2);
+});
+
+test("Square root of number", () => {
+    const result = sqrt(4,2);
+});
+
+test("Max of two numbers", () => {
+    const result = max(4,2);
+});
 
 
 
-
-})
+});
